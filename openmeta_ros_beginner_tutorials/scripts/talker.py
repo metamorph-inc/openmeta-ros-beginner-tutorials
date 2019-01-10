de3e9e8c0a8538dce03004aa7e95a054de549ecf
Joseph Coombe <jcoombe@metamorphsoftware.com>
2019-01-10 16:50:51 -0600

    Fix typo in ROS names

--- openmeta_ros_beginner_tutorials/scripts/talker.py
+++ openmeta_ros_beginner_tutorials/scripts/talker.py
@@ -40,8 +40,8 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker_topic', anonymous=True)
+    rospy.init_node('talker', anonymous=True)
+    pub = rospy.Publisher('talker_topic', String, queue_size=10)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
